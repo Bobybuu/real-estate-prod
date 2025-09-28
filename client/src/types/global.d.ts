1ce54183fbde490a5c0073ae8f2225d7e4b56819
“Chrispin <c43057772@gmail.com>
2025-09-28 21:57:33 +0300

    Corrected mapbox

---
+++ client/src/types/global.d.ts
@@ -0,0 +1,7 @@
+// Allow importing CSS files in TS
+declare module '*.css';
+declare module '*.scss';
+declare module '*.sass';
+declare module '*.svg';
+declare module '*.png';
+declare module '*.jpg'
